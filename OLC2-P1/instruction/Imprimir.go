dae43ffc4cd0d9f0b89d47ecf1362b29313c8e92
Rafael Chocoj <rafaelcliveguitar@gmail.com>
2022-03-15 10:44:27 -0600

    print array format part1

--- OLC2-P1/instruction/Imprimir.go
+++ OLC2-P1/instruction/Imprimir.go
@@ -7,6 +7,7 @@ import (
 	"OLC2/interfaces"
 	"fmt"
 	"strings"
+	//"reflect"
 
 	arrayList "github.com/colegno/arraylist"
 )
@@ -22,6 +23,53 @@ type Imprimir struct {
 func NewImprimir(val *arrayList.List, line int, column int) Imprimir {
 	exp := Imprimir{val, line, column}
 	return exp
+}
+
+func (p Imprimir) PrintArray(env interface{}, arrlist *arrayList.List) string {
+
+	array_format := ""
+	//fmt.Println("00000 arrlist: ", arrlist)
+	//fmt.Println("22222 arrlistLen(): ", arrlist.Len())
+
+	array_format = array_format + "["
+	//for _, arr := range arrlist.ToArray() {
+	for i := 0; i < arrlist.Len(); i++ {
+		arr := arrlist.GetValue(i)
+		//fmt.Println("00000 arr 			: ", arr)
+		//fmt.Println("00000	type				: ", interfaces.GetType(arr.(interfaces.Symbol).Tipo))
+		
+		
+		expre_print := arr.(interfaces.Symbol)
+		
+		if arr.(interfaces.Symbol).Tipo == interfaces.ARRAY {
+			//fmt.Println("	1111111expre_print.Tipo: 				", interfaces.GetType(expre_print.Tipo) )
+		
+
+			/*var coma = "";
+			if arr.(interfaces.Symbol).Valor.(*arrayList.List).Len() > 0 {
+				coma = ", "
+			}*/
+			getres := p.PrintArray(env, arr.(interfaces.Symbol).Valor.(*arrayList.List))
+			//fmt.Println("	1getresgetresgetres: 				", getres )
+			array_format = array_format + getres 
+		} else {
+			//fmt.Println("	1111111expre_print.Valor else: 				", expre_print.Valor )
+			
+			var coma = "";
+			if i < (arrlist.Len()-1) {
+				coma = ", "
+			}
+			if arr.(interfaces.Symbol).Tipo == interfaces.STRING || arr.(interfaces.Symbol).Tipo == interfaces.STR {
+				array_format = array_format + fmt.Sprintf("\"%v\"", expre_print.Valor)+ coma
+			} else {
+				array_format = array_format + fmt.Sprintf("%v", expre_print.Valor)+ coma
+			}
+			
+		}
+	}
+	array_format = array_format + "]" //+ ", "
+	//fmt.Println("-array_format-", array_format)
+	return array_format
 }
 
 func (p Imprimir) Ejecutar(env interface{}) interface{} {
@@ -41,7 +89,7 @@ func (p Imprimir) Ejecutar(env interface{}) interface{} {
 		//fmt.Println("format_str: ", format_str)
 		//fmt.Println("result.Tipo: ", interfaces.GetType(result.Tipo))
 
-		noformat := strings.Count(format_str, "{}")
+		noformat := strings.Count(format_str, "{}") + strings.Count(format_str, "{:?}")
 		//fmt.Println("noformat: ", noformat)
 		//fmt.Println("p.L_Expresion.Len(): ", p.L_Expresion.Len())
 
@@ -53,7 +101,20 @@ func (p Imprimir) Ejecutar(env interface{}) interface{} {
 		
 
 		for _, exp := range p.L_Expresion.ToArray() {
-			format_str = strings.Replace(format_str, "{}", fmt.Sprintf("%v", exp.(interfaces.Expresion).Ejecutar(env).Valor),1)
+			expre_print := exp.(interfaces.Expresion).Ejecutar(env)
+			//fmt.Println("-----expre_print.Tipo: ", interfaces.GetType(expre_print.Tipo) )
+			//fmt.Println("-----reflect.TypeOf(expre_print.Valor: ", reflect.TypeOf(expre_print.Valor) )
+
+			if expre_print.Tipo == interfaces.ARRAY {
+				str_arr := p.PrintArray(env, expre_print.Valor.(*arrayList.List))
+				str_arr = strings.ReplaceAll(str_arr, "][", "],[")
+				//fmt.Println("str_arr: ", str_arr)
+
+				format_str = strings.Replace(format_str, "{:?}", str_arr,1)
+
+			} else {
+				format_str = strings.Replace(format_str, "{}", fmt.Sprintf("%v", expre_print.Valor),1)
+			}
 		}
 
 		printcon := format_str + "\n"
